81a3ed161ff148b138b9fb78d8b456517e1775e8
yoosk526 <yoosk526@naver.com>
2023-08-18 08:50:18 +0000

    Create shell script

---
+++ requirement.sh
@@ -0,0 +1,8 @@
+pip install ujson    # Loading annotations faster
+pip install torchmetrics
+pip install albumentations
+pip uninstall -y opencv-python-headless
+pip install opencv-python==4.5.5.62
+pip install opencv-python-headless==4.5.5.62
+apt-get update
+apt-get install -y libgl1-mesa-glx
