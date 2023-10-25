330e8333df0af397b40da6f2689cb198f8c62f62
yoosk526 <yoosk526@naver.com>
2023-10-26 02:19:06 +0900

    train

--- model/abpn.py
+++ model/abpn.py
@@ -4,17 +4,17 @@ from model.common_block import *
 # from common_block import *
 
 class basicConv(nn.Module):    
-    def __init__(
-        self, 
-        in_channels:int,
-        out_channels:int,
-        kernel_size:int=3,
-        stride:int=1,
-        padding:int=1,
-        act:bool=True
-    ) -> None:
+    def __init__(self, 
+                 in_channels:int,
+                 out_channels:int,
+                 kernel_size:int=3,
+                 stride:int=1,
+                 padding:int=1,
+                 act:bool=True) -> None:
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.conv = nn.Conv2d(
+            in_channels, out_channels, kernel_size, stride, padding
+        )
         if act == True:
             self.act = nn.ReLU()
         else:
@@ -27,22 +27,19 @@ class basicConv(nn.Module):
         return x
 
 class ABPN(nn.Module):
-    def __init__(
-        self,
-        in_channels=3,
-        mid_channels=49,
-        out_channels=3,
-        repeat:int=5,
-        upscale:int=4,
-        normalization=False
-    ):
+    def __init__(self,
+                 in_channels=3,
+                 mid_channels=49,
+                 out_channels=3,
+                 repeat:int=5,
+                 upscale:int=4,
+                 normalization=False):
         super().__init__()
 
         if mid_channels is None:
             mid_channels = 49
         # Raise AssertionError 
         assert repeat > 2, "Repeat value should be larger than 2" 
-        
         self.upscale = upscale
 
         if not normalization:
@@ -50,22 +47,24 @@ class ABPN(nn.Module):
         else:
             self.norm = True
         
-        self.shallow = basicConv(in_channels, mid_channels)
-        buffer = []
+        self.shallow = basic_conv(in_channels, mid_channels)
+        self.deep = []
         for _ in range(repeat-1):
-            buffer.append(
-                basicConv(mid_channels, mid_channels, act=True)
+            self.deep.append(
+                basic_conv(mid_channels, mid_channels)
             )
-        buffer.append(basicConv(mid_channels, out_channels*upscale**2, act=False))
+        self.deep.append(conv_layer(mid_channels,
+                                    out_channels*upscale**2,
+                                    kernel_size=3))
         
-        self.deep = nn.Sequential(*buffer)
+        self.body = nn.Sequential(*self.deep)
         self.pixel_shuffle = nn.PixelShuffle(upscale)
 
     def forward(self, x:torch.Tensor) -> torch.Tensor:
         residual = x
         residual = torch.cat([residual for _ in range(self.upscale**2)], dim=1)      # (1, 3 * upscale**2, H, W)
         c1 = self.shallow(x)
-        c2 = self.deep(c1)
+        c2 = self.body(c1)
         c3 = c2 + residual
         c4 = self.pixel_shuffle(c3)
 
