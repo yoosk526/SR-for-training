e311790576d564d4a616ddd8c301f1081ff45059
yoosk526 <yoosk526@naver.com>
2023-11-02 17:11:36 +0900

    Add auto train code

---
+++ auto_train.sh
@@ -0,0 +1,12 @@
+# Learning rate 
+python train.py --preload --lr 0.1 --step 20 --gamma 0.5
+python train.py --preload --lr 0.01 --step 20 --gamma 0.5
+python train.py --preload --lr 0.001 --step 20 --gamma 0.5
+
+# Step
+# Epoch, Learning rate 결정되면 코드 돌리기
+python train.py --preload --lr 0.001 --step 20 --gamma 0.5
+
+# Gamma
+
+
