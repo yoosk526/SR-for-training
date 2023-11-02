caa58c239757bfa99ba6f39d2dae99a196ad7b9a
yoosk526 <yoosk526@naver.com>
2023-11-03 02:03:07 +0900

    Modify code

--- auto_train.sh
+++ auto_train.sh
@@ -1,12 +1,22 @@
 # Learning rate 
-python train.py --preload --lr 0.1 --step 20 --gamma 0.5 --epochs 
-python train.py --preload --lr 0.01 --step 20 --gamma 0.5 --epochs
-python train.py --preload --lr 0.001 --step 20 --gamma 0.5 --epochs 
+python train.py --preload --lr 0.1 --step 20 --gamma 0.5 --epochs 300
+python train.py --preload --lr 0.01 --step 20 --gamma 0.5 --epochs 300
+python train.py --preload --lr 0.001 --step 20 --gamma 0.5 --epochs 300
 
-# Step
-# Epoch, Learning rate 결정되면 코드 돌리기
-python train.py --preload --lr 0.001 --step 20 --gamma 0.5
 
-# Gamma
+# # Step
+# # Epoch, Learning rate 결정되면 코드 돌리기
+# python train.py --preload --lr  --step 10 --gamma 0.5 --epochs 
+# python train.py --preload --lr  --step 20 --gamma 0.5 --epochs 
+# python train.py --preload --lr  --step 30 --gamma 0.5 --epochs 
+# python train.py --preload --lr  --step 40 --gamma 0.5 --epochs 
+# python train.py --preload --lr  --step 50 --gamma 0.5 --epochs 
+
+# # Gamma
+# # Epoch, lr, step 결정되면 코드 돌리기
+# python train.py --preload --lr  --step --gamma 0.2 --epochs 
+# python train.py --preload --lr  --step --gamma 0.5 --epochs 
+# python train.py --preload --lr  --step --gamma 0.8 --epochs 
+
 
 
