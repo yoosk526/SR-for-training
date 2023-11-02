e057e06995dc3f67b9c648a479e58d8a6fd168e3
yoosk526 <yoosk526@naver.com>
2023-11-02 23:57:52 +0900

    Shell script for auto train

--- auto_train.sh
+++ auto_train.sh
@@ -1,7 +1,7 @@
 # Learning rate 
-python train.py --preload --lr 0.1 --step 20 --gamma 0.5
-python train.py --preload --lr 0.01 --step 20 --gamma 0.5
-python train.py --preload --lr 0.001 --step 20 --gamma 0.5
+python train.py --preload --lr 0.1 --step 20 --gamma 0.5 --epochs 
+python train.py --preload --lr 0.01 --step 20 --gamma 0.5 --epochs
+python train.py --preload --lr 0.001 --step 20 --gamma 0.5 --epochs 
 
 # Step
 # Epoch, Learning rate 결정되면 코드 돌리기
