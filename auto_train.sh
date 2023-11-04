dd7ce9e9e835e258dac5ee0d2b16dfb2a6ca326e
yoosk526 <yoosk526@naver.com>
2023-11-04 21:58:36 +0900

    Uncomment Gamma

--- auto_train.sh
+++ auto_train.sh
@@ -4,19 +4,17 @@
 # python train.py --preload --lr 0.001 --step 20 --gamma 0.5 --epochs 300
 
 
-# # Step
-# # Epoch, Learning rate 결정되면 코드 돌리기
+# Step
 # python train.py --preload --lr 0.001 --step 10 --gamma 0.5 --epochs 300
 # python train.py --preload --lr 0.001 --step 20 --gamma 0.5 --epochs 300
 # python train.py --preload --lr 0.001 --step 30 --gamma 0.5 --epochs 300
-python train.py --preload --lr 0.001 --step 40 --gamma 0.5 --epochs 300
-python train.py --preload --lr 0.001 --step 50 --gamma 0.5 --epochs 300
+# python train.py --preload --lr 0.001 --step 40 --gamma 0.5 --epochs 300
+# python train.py --preload --lr 0.001 --step 50 --gamma 0.5 --epochs 300
 
-# # Gamma
-# # Epoch, lr, step 결정되면 코드 돌리기
-# python train.py --preload --lr  --step --gamma 0.2 --epochs 
-# python train.py --preload --lr  --step --gamma 0.5 --epochs 
-# python train.py --preload --lr  --step --gamma 0.8 --epochs 
+# Gamma
+python train.py --preload --lr 0.001 --step 30 --gamma 0.2 --epochs 300
+python train.py --preload --lr 0.001 --step 30 --gamma 0.5 --epochs 300
+python train.py --preload --lr 0.001 --step 30 --gamma 0.8 --epochs 300
 
 
 
