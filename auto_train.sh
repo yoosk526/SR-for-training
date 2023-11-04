cbf83f313836f7f702fa0c15b6638d97a0a96d6c
yoosk526 <yoosk526@naver.com>
2023-11-04 07:13:27 +0000

    step

--- auto_train.sh
+++ auto_train.sh
@@ -1,16 +1,16 @@
 # Learning rate 
-python train.py --preload --lr 0.01 --step 20 --gamma 0.5 --epochs 300
-python train.py --preload --lr 0.005 --step 20 --gamma 0.5 --epochs 300
-python train.py --preload --lr 0.001 --step 20 --gamma 0.5 --epochs 300
+# python train.py --preload --lr 0.01 --step 20 --gamma 0.5 --epochs 300
+# python train.py --preload --lr 0.005 --step 20 --gamma 0.5 --epochs 300
+# python train.py --preload --lr 0.001 --step 20 --gamma 0.5 --epochs 300
 
 
 # # Step
 # # Epoch, Learning rate 결정되면 코드 돌리기
-# python train.py --preload --lr  --step 10 --gamma 0.5 --epochs 
-# python train.py --preload --lr  --step 20 --gamma 0.5 --epochs 
-# python train.py --preload --lr  --step 30 --gamma 0.5 --epochs 
-# python train.py --preload --lr  --step 40 --gamma 0.5 --epochs 
-# python train.py --preload --lr  --step 50 --gamma 0.5 --epochs 
+python train.py --preload --lr 0.001 --step 10 --gamma 0.5 --epochs 300
+python train.py --preload --lr 0.001 --step 20 --gamma 0.5 --epochs 300
+python train.py --preload --lr 0.001 --step 30 --gamma 0.5 --epochs 300
+python train.py --preload --lr 0.001 --step 40 --gamma 0.5 --epochs 300
+python train.py --preload --lr 0.001 --step 50 --gamma 0.5 --epochs 300
 
 # # Gamma
 # # Epoch, lr, step 결정되면 코드 돌리기
