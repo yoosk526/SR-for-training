a5a15b9637d479cc76b50420cbe26053d41678cb
yoosk526 <yoosk526@naver.com>
2023-11-06 04:21:23 +0900

    final gamma

--- auto_train.sh
+++ auto_train.sh
@@ -18,10 +18,10 @@ END
 # python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 40 --gamma 0.5 --epochs 300
 # python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 50 --gamma 0.5 --epochs 300
 
-Gamma
+# Gamma
 python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 30 --gamma 0.2 --epochs 300
 python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 30 --gamma 0.5 --epochs 300
 python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 30 --gamma 0.8 --epochs 300
-
-
-
+python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 40 --gamma 0.2 --epochs 300
+python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 40 --gamma 0.5 --epochs 300
+python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 40 --gamma 0.8 --epochs 300
