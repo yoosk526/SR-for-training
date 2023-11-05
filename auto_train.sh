a3cfb6b34fec44c430a618781c7b0e90801b89e3
yoosk526 <yoosk526@naver.com>
2023-11-05 21:38:11 +0900

    Modify

--- auto_train.sh
+++ auto_train.sh
@@ -3,20 +3,20 @@
     lr 0.001 / step 30 / gamma 0.2 / epochs 200
 
     *** QAT ***
-    lr  / step  / gamma  / epochs 
+    lr 0.001 / step  / gamma  / epochs 
 END
 
 # Learning rate 
-python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 20 --gamma 0.5 --epochs 300
-python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.0005 --step 20 --gamma 0.5 --epochs 300
-python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.0001 --step 20 --gamma 0.5 --epochs 300
+# python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 20 --gamma 0.5 --epochs 300
+# python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.0005 --step 20 --gamma 0.5 --epochs 300
+# python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.0001 --step 20 --gamma 0.5 --epochs 300
 
 # Step
-# python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 10 --gamma 0.5 --epochs 300
-# python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 20 --gamma 0.5 --epochs 300
-# python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 30 --gamma 0.5 --epochs 300
-# python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 40 --gamma 0.5 --epochs 300
-# python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 50 --gamma 0.5 --epochs 300
+python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 10 --gamma 0.5 --epochs 300
+python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 20 --gamma 0.5 --epochs 300
+python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 30 --gamma 0.5 --epochs 300
+python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 40 --gamma 0.5 --epochs 300
+python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 50 --gamma 0.5 --epochs 300
 
 # Gamma
 # python train.py --preload --load run/abpn-231104-22-09/weights/abpn_200.pth --qat --lr 0.001 --step 30 --gamma 0.2 --epochs 300
