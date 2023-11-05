dc5da2a93fc1be749c5b36e33922a193a992e25f
yoosk526 <yoosk526@naver.com>
2023-11-05 12:47:57 +0900

    modify

--- auto_train.sh
+++ auto_train.sh
@@ -1,5 +1,13 @@
+: <<'END'
+    *** Pretrain ***
+    lr 0.001 / step 30 / gamma 0.2 / epochs 200
+
+    *** QAT ***
+    lr  / step  / gamma  / epochs 
+END
+
 # Learning rate 
-python train.py --preload --lr 0.01 --step 20 --gamma 0.5 --epochs 300
+python train.py --preload --load run/ --lr 0.01 --step 20 --gamma 0.5 --epochs 300
 python train.py --preload --lr 0.005 --step 20 --gamma 0.5 --epochs 300
 python train.py --preload --lr 0.001 --step 20 --gamma 0.5 --epochs 300
 
